0d3d27f023b9754832595adaa58205cfad6cb8b1
Ricardo Bassete <ricardogbassete@gmail.com>
2024-05-26 17:05:46 -0300

    The exit builtin

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,8 +4,37 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
+
+type cmd struct {
+	name string
+	args []string
+}
+
+func parseCmd(str string) cmd {
+	parts := strings.Fields(str)
+	name := parts[0]
+	args := parts[1:]
+	return cmd{name, args}
+}
+
+func handleExit(cmd cmd) {
+	if cmd.name != "exit" {
+		return
+	}
+	code := 0
+	if len(cmd.args) == 1 {
+		var err error
+		code, err = strconv.Atoi(cmd.args[0])
+		if err != nil {
+			fmt.Printf("exit: %s: numeric argument required\n", cmd.args[0])
+			code = 255
+		}
+	}
+	os.Exit(code)
+}
 
 func main() {
 	for {
@@ -16,11 +45,13 @@ func main() {
 		if err != nil {
 			fmt.Println(err.Error())
 		}
-
-		run(strings.TrimRight(input, "\n"))
+		cmd := parseCmd(input[:len(input)-1])
+		run(cmd)
 	}
 }
 
-func run(command string) {
-	fmt.Fprintf(os.Stderr, "%s: command not found\n", command)
+func run(cmd cmd) {
+	handleExit(cmd)
+
+	fmt.Fprintf(os.Stderr, "%s: command not found\n", cmd.name)
 }
